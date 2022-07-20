cbaa230b0eac1782c8b71fa668d2cd445a330255
jowilf <hounonj@gmail.com>
2022-07-20 18:10:39 +0100

    road to show

--- common/templates/displays/js/json.js
+++ common/templates/displays/js/json.js
@@ -1,3 +1,1 @@
-const jsonEditor{{ name | title}} = new JSONEditor(document.getElementById("{{name}}"), {
-    mode: "view"
-},{% if data %}{{data|tojson|safe}}{% endif %});
+$("#{{name}}").append(pretty_print_json({{data | tojson |safe}}));
